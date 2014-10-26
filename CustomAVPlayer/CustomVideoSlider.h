c012379ebe7310cb57b4411a074ccf043846effb
ChengZhifeng <chengzhf_sysu@126.com>
2014-10-26 23:42:47 +0800

    添加个slider
    rt

---
+++ CustomAVPlayer/CustomVideoSlider.h
@@ -0,0 +1,13 @@
+//
+//  CustomVideoSlider.h
+//  CustomAVPlayer
+//
+//  Created by ChengZhifeng on 14-10-26.
+//  Copyright (c) 2014年 ChengZhifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomVideoSlider : UIView
+
+@end
---
+++ CustomAVPlayer/CustomVideoSlider.m
@@ -0,0 +1,21 @@
+//
+//  CustomVideoSlider.m
+//  CustomAVPlayer
+//
+//  Created by ChengZhifeng on 14-10-26.
+//  Copyright (c) 2014年 ChengZhifeng. All rights reserved.
+//
+
+#import "CustomVideoSlider.h"
+
+@implementation CustomVideoSlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
