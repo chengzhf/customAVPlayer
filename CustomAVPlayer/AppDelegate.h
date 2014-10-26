d0efa0e05f6e18ed9136bd0ad1ca6ea92c732397
ChengZhifeng <chengzhf_sysu@126.com>
2014-10-26 23:20:48 +0800

    Initial Commit

---
+++ CustomAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAVPlayer
+//
+//  Created by ChengZhifeng on 14-10-26.
+//  Copyright (c) 2014年 ChengZhifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAVPlayer
+//
+//  Created by ChengZhifeng on 14-10-26.
+//  Copyright (c) 2014年 ChengZhifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
